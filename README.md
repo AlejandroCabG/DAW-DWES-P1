2ff817f04c1cfc404b45d7a657cb26e49542e09e
alexb <acabrerag@campus.eug.es>
2022-09-21 12:51:59 +0200

    Primer commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+#### Autor: Alejandro Cabrera González ###
+
+### Solución del ejercicio
+
+El archivo README.md tiene que seguir el formato Markdown para explicar cuál ha sido
+la solución del ejercicio, las dificultades encontradas y una sección con las funcionalidades
+no conseguidas
+
+Para solucionar el ejercicio lo primero que he hecho ha sido crear tres variables: **_$shopping_cart = 5_**; 
+**_$tipo = 'periferico'_**; **_$shipping_price = 0_**.
+La primera sirve para indicar el precio de la compra; la segunda variable donde se define el tipo del objeto a comprar,
+que ser un periférico o un cable. Y la tercera variable sirve para almacenar el precio de los gastos de envío.
+Una vez definidas las variables he hecho bucles **if**, **else if** para determinar que la compra sea superior o inferior de 20€
+, que sea mayor de 50€ o de 150€, con sus respectivos **echo** para mostrar el mensaje por pantalla, y un **else** al final que 
+indica si el valor de la variable **$tipo** es incorrecta.
+### Dificultades
+Ninguna.
+### Funcionalidades no conseguidas
+Todas las funcionalidades han estado conseguidas.
--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html>
- <head>
- </head>
- <body>
-    <p>
-      <?php
-      echo "Primera página web con PHP";
-      ?>
-    </p>
-  </body>
-</html>
+<?php
+//Variables definidas
+$shopping_cart = 9.5;
+$tipo = 'periferico';
+$shipping_price = 0;
+
+//Bucle if else if
+if ($shopping_cart < 20 && $tipo == 'cable') { //En función de que el precio sea menor de 20 y del tipo especificado.
+    echo " No se puede enviar ";
+} else if ($shopping_cart < 20 && $tipo == 'periferico') {
+    $shipping_price = 4.99; //Asigno el valor de 4.99€ a la variable shipping_price.
+    echo "Los gastos de envío son $shipping_price €.";
+} else if ($shopping_cart >= 20 && $shopping_cart <= 50) { //Valores entre los cuales debe estar el precio de la compra.
+    echo "Los gastos de envío son $shipping_price €.";
+} else if ($shopping_cart > 50 && $shopping_cart <= 150) {//Se tiene que poner <= 150 para que esté incluido.
+    $shipping_price = 0;//Asigno el valor de 0€ a la variable shipping_price para indicar que es gratis.
+    echo "Los portes de envío son gratis $shipping_price €.";
+} else if ($shopping_cart > 150) {
+    echo "Código de descuento GIMBERNAT_20_%";
+} else {
+    echo "Por favor, introduzca un tipo válido: cable o periferico";
+}
+
